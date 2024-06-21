04f239600fe9218f875364b3d763d8312a2c6bde
TLectronic <3051732820@qq.com>
2024-06-21 22:38:37 +0800

    登录微服务调试成功

--- login/src/main/java/com/example/login/LoginApplication.java
+++ login/src/main/java/com/example/login/LoginApplication.java
@@ -1,9 +1,13 @@
 package com.example.login;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = "com.example.login.mapper")
 public class LoginApplication {
 
     public static void main(String[] args) {
